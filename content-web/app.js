219b65b40cb5720c58d49e7aed1f1a042858dee5
SWO-AIW <humberto.ortiz@softwareone.com>
2022-04-05 19:14:08 +0000

    Added Application Insights 2

--- content-web/app.js
+++ content-web/app.js
@@ -10,6 +10,7 @@ appInsights.setup("98f4d80d-c39e-4ac8-a384-5076497496e2");
 appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
+
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
 
